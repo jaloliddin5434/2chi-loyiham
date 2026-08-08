dd25a668e437cf3da632fef9b811af3bb2485b2f
Hazorasp Tekstil <hazorasp@tekstil.uz>
2026-08-08 08:16:50 +0500

    CI sinovi uchun ataylab buzilgan testni olib tashlash (tasdiqlash yakunlandi)

--- backend/tests/test_ci_qizil_sinov.py
+++
@@ -1,6 +0,0 @@
-"""VAQTINCHALIK - CI'ning haqiqatan xatoni ushlashini tasdiqlash uchun.
-Tasdiqlangach darhol olib tashlanadi."""
-
-
-def test_ci_atayin_muvaffaqiyatsiz():
-    assert False, "CI qizil (failed) belgi qo'yishini tekshirish uchun ataylab buzilgan test"
